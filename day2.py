bc41276bf8b0d6258db6909714b68ddbc14ae5e1
Navya <navyaburrewar2721@gmail.com>
2025-12-05 17:57:18 +0530

    if else

--- day2.py
+++ day2.py
@@ -1,1 +1,57 @@
-print("navya")
+print("Navya")
+
+### eligible for voting
+
+age =int(input())
+
+if age>18:
+    print("you are eligible for voting")
+else:
+    print("wait till you teern 18")    
+
+
+
+### price of ticket
+
+price=int(input())
+
+if price>=100:
+    print("you can buy the ticket")
+
+else:
+    print("you cant by the ticket")
+
+
+
+    ### a nad b equal are not
+
+a= int(input())
+b= int(input())
+
+if a == b:
+     print("a and b are equal")
+
+else:
+    print("a and b are not equal")
+
+#### number is negative or positive
+
+    num = int(input())
+
+if num>0:
+    print("positive")
+elif num==0:
+    print("zero")
+else:
+    print("negative")
+
+               ###### drink
+
+    num = int(input())
+
+if age<20:
+    print("don't drink")   
+elif 20 <age >50:
+    print("you may drink")
+else:
+    print("alochol is not good for your health")       
