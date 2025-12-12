2d9e897186637800e36b07253ce6e9c1e22dfbfa
Navya <navyaburrewar2721@gmail.com>
2025-12-12 23:07:28 +0530

    boolean2

---
+++ day10(boolean2).py
@@ -0,0 +1,41 @@
+##1
+a = True
+b = False
+
+print(a and b)   # False
+print(a or b)    # True
+print(not a)     # False
+
+
+### 2
+print(bool(0))       # False
+print(bool(1))       # True
+print(bool(""))      # False
+print(bool("Hi"))    # True
+print(bool([]))      # False
+print(bool([1, 2]))  # True
+
+##3 3
+def is_even(num):
+    return num % 2 == 0
+
+print(is_even(4))  # True
+print(is_even(7))  # False
+
+###4
+is_active = True
+is_logged_in = False
+
+print(is_active)      # True
+print(is_logged_in)   # False
+
+
+##x = 10
+y = 5
+x =10
+print(x > y)   # True
+print(x == y)  # False
+print(y >= 3)  # True
+
+
+
