80327a1a36ebfe46dae6b6cfad27c1154b5a3610
Navya <navyaburrewar2721@gmail.com>
2025-12-16 21:54:30 +0530

    accessingist

---
+++ day15/list3(accessing).py
@@ -0,0 +1,15 @@
+## list inside another list
+a =[1,2,3,[3,8]]
+print(a)
+
+## accessing elements  from list
+
+marks =[99,98,97,100]
+print(marks[3])
+
+print(marks[2])
+
+
+##list inside another list
+rollno=[11,15,39,[41,16]]
+print(rollno[3][1])
