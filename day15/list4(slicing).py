cb755cf2e8185f50b983554ec4ad2a90fc254a02
Navya <navyaburrewar2721@gmail.com>
2025-12-16 22:00:31 +0530

    slicing

---
+++ day15/list4(slicing).py
@@ -0,0 +1,7 @@
+number =[20,30,40,50,70,]
+print(number[0: 4])
+print(number[ :5])
+print(number[2 :])
+print(number[ :])
+print(number[4 :])
+print(number[:4])    ## first space is the stating idx and second one is the ending index
