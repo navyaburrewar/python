a51d86c091bd34b6759fa21cce990bcbca7b7ad5
Navya <navyaburrewar2721@gmail.com>
2025-12-16 22:10:28 +0530

    listmodifying

---
+++ day15/list5.py
@@ -0,0 +1,17 @@
+## negative indexing
+a=[20,80,67]
+print(a[0])
+print(a[-1])
+
+## reverse list
+print(a[:  : -1])
+
+## modifing alist
+a =[20,45,39]
+a[2]=60
+print(a)
+
+## 
+marks =[20,30,49]
+marks[2]=40
+print(marks)
