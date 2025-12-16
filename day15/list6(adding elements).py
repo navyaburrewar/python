fe3c48f884d1a3a4ae9b1fb4fa8835dc2b180504
Navya <navyaburrewar2721@gmail.com>
2025-12-16 22:24:46 +0530

    adding

---
+++ day15/list6(adding elements).py
@@ -0,0 +1,30 @@
+## adding elemnts
+### appending
+
+a =[20,30,50,38,]
+a.append(20)
+print(a)
+
+
+b =["neha","navya","akhil" ]
+b.append("nikki")
+print(b)
+
+
+
+###  extending
+
+c =["navya","choti", "20"]
+c.extend(["nandhu"])
+print(c)           ## extend will take index vice
+
+
+d =[20,30,40,50]
+d.extend([28,9,47,0])
+print(d)
+
+### insert()
+e=[20,30,40,50,60,70]
+e.insert(0,20)
+print(e)
+
