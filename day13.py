5d1fdfb8dd6db3383a20d88d4e210c505ead98bf
Navya <navyaburrewar2721@gmail.com>
2025-12-16 17:15:54 +0530

    complex

---
+++ day13.py
@@ -0,0 +1,18 @@
+z1 = 3 + 4j
+z2 = 1 - 2j
+
+print("Addition:", z1 + z2)
+print("Subtraction:", z1 - z2)
+print("Multiplication:", z1 * z2)
+print("Division:", z1 / z2)
+
+
+
+
+z = 3 + 4j
+
+if abs(z) <= 5:
+    print("Inside the circle")
+else:
+    print("Outside the circle")
+
