dcb345e98b54ad872e3b9b06a2e5a89dc5a14c41
Navya <navyaburrewar2721@gmail.com>
2025-12-17 22:59:19 +0530

    basic

--- day16(list)/operations.py
+++ day16(list)/operations.py
@@ -1,8 +1,11 @@
 a=[20,39,40]
-b=[20,30,30]
-print(a+b)
+b=[20,30,30]    ## python supports two operations in the list +,*
+print(a+b)       ## not supports the -,/, //,%,**
 print(a*2)
 print(b*2)
+
+
+
 
 
 
---
+++ day17(tuple)/basic theory.py
@@ -0,0 +1,6 @@
+##A tuple is an ordered, immutable collection of elements
+#Ordered – Elements have a fixed position (indexing starts from 0)
+#Immutable – Elements cannot be changed after creation
+#Allows duplicate values
+#Can store different data types
+#Faster than lists due to immutability
