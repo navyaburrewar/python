45ff9475cfd61f2f6dcf2520fe2a7d788b58266c
Navya <navyaburrewar2721@gmail.com>
2025-12-17 23:05:13 +0530

    created

---
+++ day17(tuple)/created(tuple).py
@@ -0,0 +1,17 @@
+t =(1,2,3)
+print(t)
+print(type(t))
+
+## 2
+## tuple without parenthese(tuple packing)
+t = 1,2,3
+print(t)
+
+## 3
+## single element tuple(comma is mandatory)
+t =(4,)
+print(t)
+
+## empty tuple
+t=()
+print(t)
