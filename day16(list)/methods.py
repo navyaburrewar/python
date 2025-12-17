0cec0ef06f38bfad8b2b571146290778976379f6
Navya <navyaburrewar2721@gmail.com>
2025-12-17 15:37:20 +0530

    method

---
+++ day16(list)/methods.py
@@ -0,0 +1,29 @@
+## methods in list 
+
+## 1
+## sorting
+
+num =[30,21,53,76,5,87]
+num.sort()
+print(num)
+
+
+a =[30,21,53,76,5,87]
+a.reverse()
+print(a)
+
+e =[30,21,53,76,5,87]
+e.count(2)
+print(e)
+
+
+
+w =[30,21,53,76,5,87]
+x =w.index(53)
+print(x)
+
+
+#### difference b/w function and method
+### a function is a block of code that performs a specific task and is not tied to any obj
+
+# method is a function that is associated with an obj or class
