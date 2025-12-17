18c4fb55bbdaa673014e537fdfb1c6a6f94cf2e1
Navya <navyaburrewar2721@gmail.com>
2025-12-17 15:24:39 +0530

    functions

---
+++ day16(list)/functions.py
@@ -0,0 +1,21 @@
+### functions in list
+
+## len(nums)
+## max(nums)
+## min(nums)
+## sum(nums)
+## sorted(nums)
+
+
+##  1
+## length
+
+d =[10,20,30,45,52,90,11]
+print(len(d))
+print(max(d))
+print(min(d))
+print(sum(d))
+print(sorted(d))
+
+
+
