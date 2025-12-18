08ef30fb31d78cd78314f686a42858d532a1bf81
Navya <navyaburrewar2721@gmail.com>
2025-12-18 11:37:07 +0530

    functions

---
+++ day18(tuple)/functions.py
@@ -0,0 +1,18 @@
+## built in tuple functions
+## len()
+## max ()
+## min()
+## sum()
+## count()
+## index()
+
+
+## 1. length
+t =(2,3,45,45)
+print(len(t))
+print(max(t))
+print(min(t))
+print(sum(t))
+
+print(t.count(2))
+print(t.index(45))
