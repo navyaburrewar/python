2e491034f59a2221c504121e12fc84225757f9b5
Navya <navyaburrewar2721@gmail.com>
2025-12-19 18:41:48 +0530

    creation-dict

---
+++ day19 (dict)/dic 2.py
@@ -0,0 +1,15 @@
+## creating a dictionary
+dict = {
+    "navya":30,
+    "aashu" :20,
+    "keerthi":30
+
+}
+print(dict)
+
+marks= {
+    "nandhu" : 40,
+    "neha":80,
+    "bhagya":0
+}
+print(marks)
