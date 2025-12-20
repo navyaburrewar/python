340bb552adfe6e7f4b0d5e9f4e7a85e05a5c487d
Navya <navyaburrewar2721@gmail.com>
2025-12-20 15:17:30 +0530

    methods

---
+++ day 20(dict)/dic 7.py
@@ -0,0 +1,18 @@
+
+## dict methods
+
+## keys()         ## returns keys
+## values()       ## returns all values
+## items()        ## key value pairs
+## updates()      ## merge dictionaries
+## copy()         ## copy dictionary
+marks ={
+    "navya" :10,
+    "pandu":20,        
+    "vivek":40
+}
+print(marks.keys())
+print(marks.values())
+print(marks.items())
+marks.update({"nandhu" : 43})
+print(marks.copy())
