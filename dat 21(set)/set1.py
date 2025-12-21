bc44c6f5cee83d6ef7257b8adf0a10c043706f9f
Navya <navyaburrewar2721@gmail.com>
2025-12-21 21:38:22 +0530

    theory-set

---
+++ dat 21(set)/set1.py
@@ -0,0 +1,7 @@
+# A set is a collection of unordered, unique elements.
+# Key Features
+# Stores only unique values
+# Unordered (no index)
+# Mutable (can add/remove elements)
+# Written using curly braces {}
+# Does not allow duplicates
