1d4752c39145964164016d3cb1aa63f873b15c5e
Navya <navyaburrewar2721@gmail.com>
2025-12-21 23:00:59 +0530

    diffrent data types

--- dat 21(set)/set2.py
+++ dat 21(set)/set2.py
@@ -1,1 +1,11 @@
 ## creating set
+## 1
+my_set = {10, 20, 30}
+
+
+##2
+my_set = set([1, 2, 3, 4])
+
+## 3
+s = set()   # correct
+s = {}      # this creates a dictionary
---
+++ dat 21(set)/set3.py
@@ -0,0 +1,4 @@
+
+## set Can store different data types
+mixed_set = {1, "hello", 3.5, True}
+print(mixed_set)
