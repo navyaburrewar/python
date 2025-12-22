8e17d747249ceed5a2d865fc55397e7e99e1c1cc
Navya <navyaburrewar2721@gmail.com>
2025-12-22 14:19:01 +0530

    functions

---
+++ dat 21(set)/set 11.py
@@ -0,0 +1,17 @@
+## set functions
+## len()
+## max(),min()
+## sum()
+## sorted ()
+## all(),any()
+#set()
+
+a = {0,1,2,3,4,53,6,84,3}
+print(len(a))
+print(max(a))
+print(min(a))
+print(sum(a))
+print(sorted(a))
+print(all(a))      ## all should be true values >0
+print(any(a))      ## anyone should be false values
+print(set(a))
