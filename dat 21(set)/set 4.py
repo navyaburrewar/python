5264b1fcb279b035c67bf38d1974f7b2b6faa781
Navya <navyaburrewar2721@gmail.com>
2025-12-22 12:41:17 +0530

    representation

---
+++ dat 21(set)/set 4.py
@@ -0,0 +1,10 @@
+##  important points
+
+#set representation
+set ={1,2,3}
+set =set[(1,2,3,4)]
+set=set()  ### empty set
+
+## we do not reprent as the 
+set =(1,2,3)  ## which reprents a tuple
+set ={}  ## which is an empty  dictionaty
--- dat 21(set)/set3.py
+++ dat 21(set)/set3.py
@@ -1,4 +1,4 @@
 
 ## set Can store different data types
 mixed_set = {1, "hello", 3.5, True}
-print(mixed_set)
+print(mixed_set)                  ## in out put we will not get the true beacuse true is treated as a 1s
