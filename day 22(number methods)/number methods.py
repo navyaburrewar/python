60ed9f85dc0b013ec6bee02c18d9440224e4f2eb
Navya <navyaburrewar2721@gmail.com>
2025-12-22 23:35:47 +0530

    number methods

---
+++ day 22(number methods)/number methods.py
@@ -0,0 +1,10 @@
+#These are called using dot notation
+
+#bit_length()
+#How many bits are needed to store the number.
+x = 10
+print(x.bit_length())   
+
+#bit_count()
+#Counts 1s in binary form.
+print((10).bit_count())
