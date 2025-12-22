012e771932bf8b10f394bd2104262059b76c253e
Navya <navyaburrewar2721@gmail.com>
2025-12-22 23:26:05 +0530

    math-module-functions

---
+++ day 22(number methods)/math module function.py
@@ -0,0 +1,21 @@
+## here we must import math
+
+#math.ceil() – rounds UP
+import math
+print(math.ceil(3.2)) 
+
+
+#math.floor() – rounds DOWN
+print(math.floor(3.9))  
+
+
+#math.sqrt() – square root
+print(math.sqrt(16))    
+
+##math.exp() – e to the power x (eˣ)
+print(math.exp(3))        # (eˣ)
+
+
+#math.log() – natural log (base e)
+print(math.log(10))
+
