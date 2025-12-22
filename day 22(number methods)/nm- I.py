33796bbe0837c07507601dabab94b127127a664e
Navya <navyaburrewar2721@gmail.com>
2025-12-22 23:18:39 +0530

    number methods

---
+++ day 22(number methods)/nm- I.py
@@ -0,0 +1,23 @@
+
+# Built-in Number Functions
+
+#abs() – absolute value
+#Removes the negative sign.
+x = -10
+print(abs(x))   
+
+#round() – rounds a number
+#Rounds to the nearest whole number (or decimals).print(round(3.6))      # 4
+
+print(round(3.6))      
+print(round(3.14159, 2))   ## second decimal 
+
+#max() – largest value
+# min
+print(max(2, 5, 1, 9))   
+print(min(2, 5, 1, 9))
+
+##pow()
+#pow() – power (exponent)
+print(pow(2, 3))
+print(pow(2, 3))
