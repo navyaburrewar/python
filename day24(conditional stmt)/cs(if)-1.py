3b8c097867807855eabbeea4041843052c3c47fb
Navya <navyaburrewar2721@gmail.com>
2025-12-23 22:59:07 +0530

    if-condition

---
+++ day24(conditional stmt)/cs(if)-1.py
@@ -0,0 +1,16 @@
+#IF Statement
+
+#The if statement executes a block of code only if the condition is true.
+
+#Syntax:
+
+# if (condition) {
+# //code
+# }
+
+# ex-1
+if (4>9):
+    print("correct")     # ouput will not print any thing
+
+if(3<9):
+    print("correct")     ## ouput will be correct
---
+++ day24(conditional stmt)/cs(types).py
@@ -0,0 +1,10 @@
+#Types of Conditional Statements
+
+#Conditional statements are mainly of the following types:
+
+    #if statement
+    #if–else statement
+    #if–else–if ladder
+    #Nested if statement
+    #switch statement (in some languages)
+    #Ternary (Conditional) Operator
