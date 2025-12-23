4d677d80677f03ccec8de4c356a12e18592c35de
Navya <navyaburrewar2721@gmail.com>
2025-12-23 14:44:56 +0530

    comparision

---
+++ day23(operators).py/comparison 3.py
@@ -0,0 +1,19 @@
+#Comparison (Relational) Operations
+#Used to compare two values.
+#Result is True or False.
+#==	Equal to
+#!=	Not equal
+#>	Greater than
+#<	Less than
+#>=	Greater than or equal
+#<=	Less than or equal
+
+a = 10
+b = 5
+
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a>=b)
+print(a<=b)
