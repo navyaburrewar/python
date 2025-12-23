5005a3466a943ecee8a6cb96ef198137951d5e0d
Navya <navyaburrewar2721@gmail.com>
2025-12-23 14:52:27 +0530

    bitwise

---
+++ day23(operators).py/bitwise 5.py
@@ -0,0 +1,19 @@
+#Bitwise Operation
+#Works on binary numbers.
+
+#&	AND
+#`	`
+#^	XOR
+#~	NOT
+#<<	Left shift
+#>>	Right shift
+a = 5   # binary 0101
+b = 3   # binary 0011
+
+
+print(a & b)
+print(a | b)        #Converts numbers into binary
+print(a ^ b)        #Performs bit-by-bit operation
+
+
+                               
