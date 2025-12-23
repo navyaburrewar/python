b0d8a938fa01a0175d69989969a6cd75713f4927
Navya <navyaburrewar2721@gmail.com>
2025-12-23 14:46:56 +0530

    logical

---
+++ day23(operators).py/logical 4.py
@@ -0,0 +1,12 @@
+#Logical Operations
+#Used to combine conditions.
+
+#and	True if both conditions are true
+#or	    True if at least one is true
+#not	Reverses result
+a = 10
+b = 5
+
+print(a > 5 and b < 10)
+print(a > 5 or b > 10)
+print(not(a > 5))
