0286a496a10e0ed895bb9f9b171112070a9e7c4f
Navya <navyaburrewar2721@gmail.com>
2025-12-23 14:42:05 +0530

    assignment

---
+++ day23(operators).py/assignment 2.py
@@ -0,0 +1,17 @@
+#Assignment Operations
+#Used to assign values to variables.
+# =  # assign
+#+=  ## addintion
+# -= ## sub the value
+# *=  pow of value
+# /= divides value
+
+x = 10
+x += 5
+print(x)
+x-=5
+print(x)
+x*=3
+print(x)
+x/=2
+print(x)
