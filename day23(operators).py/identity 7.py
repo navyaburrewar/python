96ca3d743e58cb590f322a9849f2510f863baee4
Navya <navyaburrewar2721@gmail.com>
2025-12-23 14:56:46 +0530

    identity

---
+++ day23(operators).py/identity 7.py
@@ -0,0 +1,14 @@
+#Identity Operations
+#Checks if two variables refer to the same objecta = 10
+a=10
+b = 10
+
+print(a is b)
+print(a is not b)
+
+## ex-2
+a=100
+b = 10
+
+print(a is b)
+print(a is not b)
