a270f5eff53a0682892ba628041f8d9d7224889d
Navya <navyaburrewar2721@gmail.com>
2025-12-24 22:21:22 +0530

    neasted-if

---
+++ day24(conditional stmt)/cs(neasted)-6.py
@@ -0,0 +1,60 @@
+## 1
+## positive and even number
+#num=int(input())
+# if num>0:
+#     if num%2==0:
+#         print("positive and even number")
+#     else:
+#         print("positive and odd number")
+# else:
+#     print("negative number")            
+
+
+
+
+
+
+
+
+#2
+
+# #Student Pass & Grade System
+# num =int(input())
+# if  num>30:
+#     if num>90:
+#         print("o")
+#     elif 90<num>60:
+#         print("a")
+#     elif 60<num>40:
+#         print("b")
+# else:
+#     print("fail")       
+
+
+
+## 3
+##Age Verification
+## children or adult or senior citiznum 
+# age=int(input())
+# if age<60:
+#   if age<20:
+#       print("children")
+#   else:
+#       print("adults")   
+# else:
+#     print("senior citizens")       
+
+
+
+
+##4
+# Nested if with Multiple Conditions
+# username="navya"
+# password="2721"
+# if username=="navya":
+#     if password=="2721":
+#         print("login successful")
+#     else:
+#         print("login fail")
+# else:
+#     print("wrong username")            
