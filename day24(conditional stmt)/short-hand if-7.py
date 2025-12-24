2b6db60b39ba2eac1a21ccfa09bac0266db6507e
Navya <navyaburrewar2721@gmail.com>
2025-12-24 22:29:42 +0530

    short-hand if

---
+++ day24(conditional stmt)/short-hand if-7.py
@@ -0,0 +1,16 @@
+#Short-Hand if (Ternary Operator)
+#Used for single-line conditions
+
+
+#syntax
+# value_if_true if condition else value_if_false
+
+
+## 1
+n=20
+print("even")if n%2==0 else("odd")
+
+## 2
+m=30
+n=50
+print("greater")if m>n else print("small")
