bd2fadbc4b178c80998ec1759d53e3768432071a
Navya <navyaburrewar2721@gmail.com>
2025-12-24 12:59:53 +0530

    if-else

---
+++ day24(conditional stmt)/cs(if-else)-3.py
@@ -0,0 +1,14 @@
+#if else statement
+#If condition is true do this, otherwise do that
+
+# syntax
+
+# if (condition):
+#     #statment
+# else:
+#     # statment    
+age =int(input())
+if age>20:
+    print ("you can eligible to vote")
+else:
+    print("not eligible for vote")    
