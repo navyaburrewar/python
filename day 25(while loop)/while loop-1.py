a93a219d060db62171fa0d3e274103f6c590a5f0
Navya <navyaburrewar2721@gmail.com>
2025-12-25 15:29:13 +0530

    theory

---
+++ day 25(while loop)/while loop-1.py
@@ -0,0 +1,27 @@
+#What is a while loop?
+
+#A while loop is used to repeat a block of code as long as a condition is True.
+#You don’t know in advance how many times the loop will run
+#The loop depends on a condition
+
+# syntax
+
+# while condition:
+    #code to be executed
+
+
+#Important points:
+
+#condition must be True or False
+#The code inside the loop must be indented
+#The loop stops when the condition becomes False
+
+
+
+
+# Flow of execution (how it works)....
+
+# Python checks the condition
+# If the condition is True → loop body executes
+# After execution, Python again checks the condition
+# This repeats until the condition becomes False
