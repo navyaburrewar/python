5b0d4a630b8cd17ed99177ec956221d113f59e34
Navya <navyaburrewar2721@gmail.com>
2025-12-25 16:00:40 +0530

    problems

---
+++ day 25(while loop)/while loop-2.py
@@ -0,0 +1,34 @@
+# #3 ex-1
+
+# i=10
+# while i<20:
+#     print(i)
+#     i=i+1
+
+
+# ## infinate while loop
+# #If the condition never becomes False, the loop runs forever.     
+
+# while True:
+#  print("navya")
+
+
+
+  ## here will user input and also increment or choosing your own num 
+# num =int(input("enter a number: "))    
+# while num<90:
+#     print(num)
+#     num =int(input("enter a number: ")) 
+
+
+ 
+
+# brak will stop the loop
+
+i=1
+
+while i<=10:
+    if i==5:
+        break
+    print(i)
+    i=i+1
