44b8161b8450909759f58ca09b349bd5746d07dd
Navya <navyaburrewar2721@gmail.com>
2025-12-26 14:19:08 +0530

    problems on break

---
+++ day 25(while loop)/break-continue-4.py
@@ -0,0 +1,92 @@
+#Write a program using a while loop to print numbers from 1 to 20, but stop the loop if the number becomes 13.
+
+# i=1
+# while i<=20:
+
+#      if i==13:
+#       break
+#      print(i)
+#      i=i+1 
+
+
+
+## 2
+##Write a program that keeps taking numbers from the user and stops when the user enters 0.
+
+# while True:
+    
+#     i=int(input())
+#     if i==0:
+#         break
+#     print(i)
+
+
+# 3
+# Write a program that repeatedly asks the user for a password and stops only when the correct password is entered.
+# password ="choti"
+# num =input("enter password: ")
+# while num==password:
+#     if password!="choti":
+#         break
+# print("wrong password")
+# num=input("enter password")
+         
+
+## 4
+# ## Keep asking the user to enter a number. Stop the loop when the user enters a negative number.         
+# while True:
+#     i=int(input())
+#     if i<0:
+#      break   
+
+
+
+## 5
+# Stop when the sum exceeds 100
+
+# sum=0
+# while sum<100:
+#      m =int(input())
+#      sum =sum+m
+# print("sum exceeda 100")
+     
+
+# 6
+#Stop when the user types "exit"
+
+# text="exit"
+# ch=input("char: ")   
+# while ch!=text:
+    
+#     print(ch)
+#     ch=input("char: ")
+
+
+ # 7   
+#Stop when marks entered is -1 
+# marks =-1
+# num=int(input())
+# while num!=marks:
+#      print(num)
+#      num=int(input())
+
+
+
+# ##8
+       
+    
+   #9 
+ # atm pin check
+correct_pin =1234
+pin =int(input())
+while pin!=correct_pin:
+        print("wrong passwors")
+        pin=int(input())
+        pin==correct_pin
+        print("password is correct")
+
+      
+   
+    
+
+    
