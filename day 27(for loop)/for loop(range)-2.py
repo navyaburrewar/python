771d80d353a9615a663958296d86b6c51f314f0f
Navya <navyaburrewar2721@gmail.com>
2025-12-27 15:42:19 +0530

    range-forloop

---
+++ day 27(for loop)/for loop(range)-2.py
@@ -0,0 +1,19 @@
+## range in for loop
+
+# syntax
+
+#range(stop)
+#range(start, stop)
+#range(start, stop, step)
+
+##ex-1
+for i in range(1,9):
+    print(i)                 ## starting will be starting
+                             ## ending will leass than 1
+
+
+## ex-2
+for i in range(2,20,2):
+    print(i)                                
+
+
