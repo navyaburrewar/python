3563e4cedc2dbfce7c44aec5120bf36c33aea71b
Navya <navyaburrewar2721@gmail.com>
2025-12-30 23:53:16 +0530

    problem-2

---
+++ day 27(for loop)/day 30/probelms-2.py
@@ -0,0 +1,10 @@
+#5. Print numbers from 10 to 1
+for i in range(10,0,-1):
+     print(i)
+
+
+
+## 6  Multiplication table of a number
+n =int(input("enter a number: "))
+for i in range(1,11):
+     print(n ,"x", "=", n*i)
