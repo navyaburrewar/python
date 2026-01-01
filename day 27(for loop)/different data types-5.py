3dd5a2e2b2413302afa8b0188e6c04937a2fa730
Navya <navyaburrewar2721@gmail.com>
2026-01-01 22:54:34 +0530

    patterns

---
+++ day 27(for loop)/different data types-5.py
@@ -0,0 +1,8 @@
+##Iterating over different data types
+
+# Python for loops can iterate over:
+# List: for item in [1, 2, 3]:
+# Tuple: for item in (1, 2, 3):
+# String: for char in "hello":
+# Dictionary: for key in dict:
+# Set: for item in {1, 2, 3}:
---
+++ day31/printing stars.py
@@ -0,0 +1,19 @@
+## printing stars
+
+## print a single star
+
+for i in range(1,2):
+    print("*")
+
+
+
+## Print 5 stars in one line.
+for i in range(1,6):
+    print("*" ,end="")
+
+
+
+
+## Print 5 stars, each on a new line.
+for i in range(1,7):
+    print("*")
