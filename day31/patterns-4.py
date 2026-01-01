84e52f9044780fae5c05d5e8044291891957fe1b
Navya <navyaburrewar2721@gmail.com>
2026-01-02 00:11:43 +0530

    patterns

---
+++ day31/patterns-4.py
@@ -0,0 +1,17 @@
+## Triangle using row number (1 to 4)
+for i in range(0,5):
+      print("*"*i)
+
+
+##  Triangle using single loop & repetition
+for i in range(1, 6):
+    print("*" * i)
+
+
+##
+n=5
+for i in range(1,n+1):
+     print(" "*(n-i),"*"*i)
+
+  
+
