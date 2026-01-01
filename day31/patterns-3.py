677e77fd0b6d983aa98674a40b418631a90c7bb6
Navya <navyaburrewar2721@gmail.com>
2026-01-01 23:40:48 +0530

    inverted right angle triangle

---
+++ day31/patterns-3.py
@@ -0,0 +1,11 @@
+## Inverted right-angled triangle
+
+for i in range(5,0,-1):
+    for j in range(i-1):
+        print("*",end="")
+    print()
+
+
+### another way
+for i in range(5,0,-1):
+      print("*"*i)
