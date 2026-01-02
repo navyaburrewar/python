c2d1a2623a38db7f4a1f3b7fa59c9358dd064720
Navya <navyaburrewar2721@gmail.com>
2026-01-02 22:11:58 +0530

    level-2

---
+++ day32(patterns)/level-2.py
@@ -0,0 +1,63 @@
+##   11  Print a pattern of 3 rows and 6 columns of *
+
+for i in range(3):
+    print("*"*6)
+
+
+
+## 12  Print a square of size n (user input)
+
+n=int(input())
+for i in range(n):
+    print("*"*n)
+
+
+## 13       Print a right-angled triangle with *
+for i in range(5):
+    print("*"*i)
+
+
+
+## 14  Print a triangle with 5 rows
+
+for i in range(6):
+    print("*"*i)
+
+
+
+## 15 Print a triangle using user input n
+n =int(input())
+for i in range(n):
+    print("*"*i)
+
+
+
+## 16  Print a triangle with increasing stars
+n =int(input())
+for i in range(n):
+    print("*"*i)
+
+
+## 17  Print a triangle starting from 1 star to 6 stars
+n =int(input())
+for i in range(n):
+    print("*"*i)
+
+
+
+## 18 Print a triangle with each row having row-number stars
+
+n=int(input())
+for i in range(n):
+    print("*"*i)
+
+
+
+## 19 Print a triangle aligned to the left
+for i in range(4):
+    print("*"*i)    
+
+
+## 20   Print a triangle aligned to the left
+for i in range(6):
+    print("+"*i)
