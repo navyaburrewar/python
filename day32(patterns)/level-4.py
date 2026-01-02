68fdf5ed63494bdc1b6a9b997bb7cb43bf49415b
Navya <navyaburrewar2721@gmail.com>
2026-01-02 23:08:20 +0530

    level-5

---
+++ day32(patterns)/level-4.py
@@ -0,0 +1,12 @@
+##  29   Print an inverted triangle using numbers
+
+for i in range(1,9):
+    print("1"*i)
+
+
+## 30    Print an inverted triangle using numbers
+for i in range(10,0,-1):
+    for j in range(i):
+        print(i,end="")
+    print()    
+  
