e9e6733d63a36edb3587917a75910f0c848d417a
Navya <navyaburrewar2721@gmail.com>
2026-01-02 21:43:12 +0530

    level-1

---
+++ day32/patterns problems-5.py
@@ -0,0 +1,62 @@
+#  1  Print a square of * with 5 rows and 5 columns
+for i in range(6):
+    print("*"*6)
+
+
+## 2   Print a square of * with 5 rows and 5 columns
+for i in range(6):
+    for j in range(6):
+        print("*",end="")
+    print()        
+
+
+
+## 3 Print a single row of 10 *
+for i in range(10):
+    print("*",end="")
+
+
+
+##   4 Print a single column of 5 *
+for i in range(6):
+    print("*")
+
+
+
+##   5  Print a square of numbers 1
+for i in range(4):
+    print("1"*4)
+
+
+
+##   6  Print a square of numbers 1
+for i in range(4):
+    for j in range(4):
+        print("1",end="")
+    print()
+
+
+
+## 7          Rectangle 3×5
+
+for i in range(3):
+    for j in range(5):
+        print("*",end="")
+    print()        
+
+
+## 8  rectangle 3*5
+
+for i in range(3):
+    print("*"*5)      
+
+
+## 9 Print a 4×4 square using #
+
+for i in range(4):
+    print("#"*4)
+
+
+## 10    Print a square using numbers 1 1 1 1
+for i in range(4):
+    print("1"*4)
