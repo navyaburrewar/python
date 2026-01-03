06554e269aad0f81a581e08ca54df07a9a08c7eb
Navya <navyaburrewar2721@gmail.com>
2026-01-03 23:07:07 +0530

    level-6

---
+++ day33/level 6.py
@@ -0,0 +1,21 @@
+##   31  Print numbers from 1 to n in triangle form
+for i in range(1,5):
+    for j in range(1,1+i):
+        print(j,end="")
+    print()    
+
+
+## 32 Print a triangle with same number 
+for i in range(5):
+    for j in range(i*1):
+        print("1", end="")
+    print()    
+
+
+
+
+## 32 Print a triangle with same number in each row
+for i in range(5):
+    for j in range(i*1):
+        print(i, end="")
+    print()    
