1cf34759275b2cee7a9e66bdd5f8b4814bd74370
Navya <navyaburrewar2721@gmail.com>
2026-01-05 19:05:40 +0530

    alphabet

--- day34  alphabet/triangle alphabet-5.py
+++ day34  alphabet/triangle alphabet-5.py
@@ -12,3 +12,11 @@ for i in range(4):
     for j in range(4):
         print(chr(65+i), end=" ")
     print()        
+
+
+
+n=4
+for i in range(n):
+    for j in range(i+1):
+        print(chr(65+i),end=" ")
+    print()        
