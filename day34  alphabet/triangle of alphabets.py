c9b90028702b9b629709d1f79cc604ffdcb20732
Navya <navyaburrewar2721@gmail.com>
2026-01-05 17:51:36 +0530

    triangle of alphabet

---
+++ day34  alphabet/triangle of alphabets.py
@@ -0,0 +1,33 @@
+### traingle of alphabets 
+## ASCII stands for American Standard Code for Information Interchange.
+
+#  ASCII values for Alphabets
+## Uppercase Alphabets (A–Z)
+
+## letter                    ascii value
+# A                            65
+# B                            66
+# .
+# .
+# Z                            90
+
+
+
+
+##3 LOWERCASE  alphabet
+## a                        97
+## b                        98
+## .
+##  .
+## .
+## z                        122
+
+
+
+print(ord('c'))
+print(ord('A'))
+
+
+
+print(chr(65))
+print(chr(66))
