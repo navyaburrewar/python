696a413e4abda1446e243415dbbb40cc347e6e77
Navya <navyaburrewar2721@gmail.com>
2026-01-05 18:03:23 +0530

    triangle of alphabet

---
+++ day34  alphabet/triangle of alphabet-2.py
@@ -0,0 +1,19 @@
+## Print a triangle of alphabets
+
+n=6
+for i in range(n):
+    for j in range(i+1):
+        print(chr(65+i), end= " ")
+    print()    
+
+
+
+n=6
+for i in range(n):
+    for j in range(i+1):
+        print(chr(65+j), end= " ")
+    print()
+
+
+
+
