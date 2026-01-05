2dbf37a8a4bc7a66067963a793b5e77f050b20f0
Navya <navyaburrewar2721@gmail.com>
2026-01-05 19:01:04 +0530

    alphabet square

---
+++ day34  alphabet/triangle alphabet-5.py
@@ -0,0 +1,14 @@
+## Print alphabet square
+for i in range(4):
+    for j in range(4):
+        print(chr(65+j), end=" ")
+    print()    
+
+
+
+
+## Print alphabet square
+for i in range(4):
+    for j in range(4):
+        print(chr(65+i), end=" ")
+    print()        
