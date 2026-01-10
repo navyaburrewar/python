4c98be1825f678e38e4d580e8b21ee9d829e0383
Navya <navyaburrewar2721@gmail.com>
2026-01-10 23:41:15 +0530

    pyramid

--- day 36(pyramid)/pyramid-5.py
+++ day 36(pyramid)/pyramid-5.py
@@ -7,12 +7,5 @@ for i in range(n):
     print()    
 
 
-## full star pyramid 
-
-n=6
-num=1
-for i in range(n):
-    for j in range(i+1):
-        print(" "*(n-i)+(2*i-1))
 
     
---
+++ day 36(pyramid)/pyramid-6.py
@@ -0,0 +1,5 @@
+#mid using user input
+
+n=int(input())
+for i in range(n):
+    print(" "*(n-i)+"*"*(2*i-1))
