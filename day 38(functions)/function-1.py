123b00bbffbd1df42de2641834d0e40abd3d1d7d
Navya <navyaburrewar2721@gmail.com>
2026-01-12 22:45:42 +0530

    theory

---
+++ day 38(functions)/function-1.py
@@ -0,0 +1,24 @@
+##  thory on functions
+
+## A function is a block of code that:
+
+#Has a name
+#Does a specific task
+#Can be used again and again
+#Instead of writing the same code 10 times, you write it once in a function and call it whenever needed.
+def greet():
+    print("Hello!")
+greet()    
+
+
+
+ ## where we use the  functions
+
+ # Avoid writing the same code again and again
+# Make programs easier to read
+# Make programs easier to fix
+# Break big problems into small parts
+# Save time
+def say_hi():
+    print("Hi, welcome to Python!")
+say_hi()   
