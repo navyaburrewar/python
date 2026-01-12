db56523188b0e8d1a4be949bbfb2d04f1c2b699f
Navya <navyaburrewar2721@gmail.com>
2026-01-12 23:16:08 +0530

    function-4

---
+++ day 38(functions)/functions-4.py
@@ -0,0 +1,28 @@
+## why do we use the parameters
+
+
+## without parameters
+
+def add():
+    print(10+30+3)
+add()    
+
+## this is fixed
+
+## with parameters
+
+def add(a,b):
+    print(a+b)
+add(2,8)    
+## it works for any number
+
+## so from about two differnt code you will know that
+
+
+#When do we use parameters?
+
+#--> We use parameters when:
+# The function must work with different inputs
+# We want to make our code flexible
+# We want to avoid hard-coding values
+# We need user input or dynamic data
