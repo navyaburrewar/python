80eb5110d5820d614567e716c1f6153c193740bf
Navya <navyaburrewar2721@gmail.com>
2026-01-12 23:08:14 +0530

    using 1-parameter

--- day 38(functions)/function-2.py
+++ day 38(functions)/function-2.py
@@ -8,3 +8,14 @@
 def atmosphere():
     print("it was it very hot")
 atmosphere()    
+
+
+
+##from about 
+## def  --> tells Python you are creating a function
+## atmosphere --> function name
+##()   -->      
+# :--->start of function body
+## indented code --> belong to the function code
+
+## calling is atomshere() by using this we will call
---
+++ day 38(functions)/function-3.py
@@ -0,0 +1,20 @@
+##  function with input parameters 
+
+
+## defination of parameter
+## --> A parameter is a variable defined inside a function that receives a value when the function is called.
+
+## syntax for the parameter:
+
+## def greet(name):
+#     code
+
+def greet(name):
+    print("welcome",name)
+greet("choti")    
+
+
+# --> name is an parameter
+# --> choti is an arguments
+
+
