39f01f553f34da17758895f17aa0c692d7956bf0
Navya <navyaburrewar2721@gmail.com>
2026-01-13 18:19:04 +0530

    glocal & local variables

--- day 39(functions)/function-7.py
+++ day 39(functions)/function-7.py
@@ -1,1 +1,6 @@
 ### difference between the print() and returtn
+
+# print()	                return
+# Shows output	        Sends value
+# Can’t be stored	    Can be stored
+# Only for display	    Used in calculations
---
+++ day 39(functions)/function-8.py
@@ -0,0 +1,17 @@
+### local and global variables
+
+x=10              ## global variable
+def show():
+    y=30
+    print(x)         ## local variable
+    print(y)
+
+
+
+
+
+
+
+
+
+
