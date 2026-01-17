c9f198680be658046de166e6a89561b4442efb64
Navya <navyaburrewar2721@gmail.com>
2026-01-17 18:11:59 +0530

    theory

---
+++ day 44/args and kwargs-2.py
@@ -0,0 +1,7 @@
+##  *args and **kwargs
+
+## By default, a function must be called with the correct number of arguments.
+#However, sometimes you may not know how many arguments that will be passed into your function.
+#*args and **kwargs allow functions to accept a unknown number of arguments.
+
+
