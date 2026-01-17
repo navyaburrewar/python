952acf8ece8426e4dd0f448ab7423ed8456a16fb
Navya <navyaburrewar2721@gmail.com>
2026-01-17 18:43:47 +0530

    accessing individual arguments

---
+++ day 44/accessing individual arguments.py
@@ -0,0 +1,6 @@
+def func(*rank):
+    print("first rank =" + rank[0])
+    print("second rank =" +rank[1])
+    print("third rank =" +rank[2])
+    print("fourth rank=" +rank[3])
+func("1","2","3","4")
