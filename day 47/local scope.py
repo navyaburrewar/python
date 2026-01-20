f728e952fcaa8112948a930e0cef2dc5c2a08eb3
Navya <navyaburrewar2721@gmail.com>
2026-01-20 14:07:18 +0530

    local scope

---
+++ day 47/local scope.py
@@ -0,0 +1,14 @@
+## local scope
+#A variable created inside a function belongs to the local scope of that function, and can only be used inside that function.
+
+def func():
+    x=10                ## avriable is inside the function
+    print(x)
+func()    
+
+
+
+def my_func():
+    y=20
+    print()
+my_func()    
---
+++ day 47/scope.py
@@ -0,0 +1,10 @@
+## scope
+##A variable is only available from inside the region it is created. This is called scope.
+## real life example :-
+#  A whiteboard is inside one classroom.
+
+# Only students and the teacher in that classroom can see and use that whiteboard.
+# Students in other classrooms cannot see or use it.
+# 👉 The classroom is the scope.
+# 👉 The whiteboard is the variable.
+
