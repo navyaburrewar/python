08f3c0b9e4f200068e20f8c4801b801bc37f1ae2
Navya <navyaburrewar2721@gmail.com>
2026-01-21 19:31:54 +0530

    basic of decorate

---
+++ day 48(decorators)/basic-1.py
@@ -0,0 +1,17 @@
+### python decorators
+
+
+##Decorators let you add extra behavior to a function, without changing the function's code.
+#A decorator is a function that takes another function as input and returns a new function.
+
+
+
+
+### real world  explantion of this :--
+
+###Think of a decorator like gift wrapping 🎁
+# ur function
+# The wrapping = extra behavior
+# The gift inside does not change, it just looks better or does more
+# So:
+# A decorator adds extra work to a function without changing the function itselfThe gift = yo
