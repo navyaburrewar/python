ecee1000495a25cf53e256d3852cc93e23920617
Navya <navyaburrewar2721@gmail.com>
2026-01-25 23:10:09 +0530

    even or odd

---
+++ day 51/q-4.py
@@ -0,0 +1,10 @@
+# Write a function that takes a number and returns "Even" or "Odd".
+
+def func(a):
+      if a%2==0:
+            return "even"
+      else:
+            return "odd"
+func(20) 
+print(func(20))
+        
