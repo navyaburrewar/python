91593ea40ef6d1a703c8a1358fd9e3a7b5f7787f
Navya <navyaburrewar2721@gmail.com>
2026-01-26 20:26:07 +0530

    functions

---
+++ day 52.py/q-1.py
@@ -0,0 +1,8 @@
+def func(a):
+      if a%2==0:
+            return "even"
+      else:
+            return "odd"
+func(20) 
+print(func(20))
+        
--- interview.py
+++ interview.py
@@ -6,42 +6,34 @@
 
 # # heads=35
 # # legs=94
-# # ## chiken =1 head + 2 legs
-# # ## rabit = 1 head +4 legs
-# # rabit=[0]
-# # chiken=[0]
-# # i="head"
-# # j="leg"
-# # for i in range(35+1):   ## i = head
-# #     for j in range(94+1):   ## j = leg
-# #         if i==1 & j==2:
-# #             chiken = chiken+1
-# #             print(count(chiken))
+# ## chiken =1 head + 2 legs
+# ## rabit = 1 head +4 legs
+# rabit=[0]
+# chiken=[0]
+# i="head"
+# j="leg"
+# for i in range(35+1):   ## i = head
+#     for j in range(94+1):   ## j = leg
+#         if i==1 & j==2:
+#             chiken = chiken+1
+#             print(count(chiken))
 
-# #         elif i==1 & j==4:
-# #             rabit= rabit+1
-# #             print(count(rabit))
+#         elif i==1 & j==4:
+#             rabit= rabit+1
+#             print(count(rabit))
            
-# ## Write a python code to accept a string and count the number of vowels and consonants.
-# # 
-# i = "navya"
-# vowels= "a" or "e" or "i"or "o" or "u"
+## Write a python code to accept a string and count the number of vowels and consonants.
+# 
+i = "navya"
+vowels= "a" or "e" or "i"or "o" or "u"
 
-# print("vowels", i.count(vowels))
-# print("consonants"i.count())    
+print("vowels", i.count(vowels))
+print("consonants"i.count())    
 
-#     ##Write a program to find the number of occurrences of elements from list1 to list2
+    ##Write a program to find the number of occurrences of elements from list1 to list2
 
 
 
-heads = 35
-legs = 94
-
-for chickens in range(heads + 1):
-    rabbits = heads - chickens
-    if 2*chickens + 4*rabbits == legs:
-        print("Chickens:", chickens)
-        print("Rabbits:", rabbits)
 
         
 
