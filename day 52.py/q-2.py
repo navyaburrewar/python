975219091ce28e0d59cf9b4a68099cd7c89436db
Navya <navyaburrewar2721@gmail.com>
2026-01-27 23:05:17 +0530

    function

--- day 52.py/q-2.py
+++ day 52.py/q-2.py
@@ -1,7 +1,13 @@
-def func(a):
-      if a%2==0:
-            return "even"
-      else:
-            return "odd"
-func(20) 
-print(func(20))
+# Define a function
+def square_number(number):
+    result = number * number
+    return result
+
+# Take input from user
+num = float(input("Enter a number: "))
+
+# Call the function
+output = square_number(num)
+
+# Display result
+print("The square of", num, "is", output)
