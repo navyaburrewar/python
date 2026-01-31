4d1e0a80ca879b048d37c8ad01666608a65be0d7
Navya <navyaburrewar2721@gmail.com>
2026-01-31 19:08:37 +0530

    lambda function

---
+++ day53/lambda function).py
@@ -0,0 +1,9 @@
+##  use lambda function
+
+## Why Use Lambda Functions in Python?
+# A lambda function is used when you need a small, simple function for a short time, especially when working with other functions.
+# It helps make your code:
+
+# Shorter
+# Cleaner
+#Easier to write in certain situations
