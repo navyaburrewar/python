602565fcd14bb6ff5909f72da252fac232e4b5b0
Navya <navyaburrewar2721@gmail.com>
2026-01-31 18:14:11 +0530

    function

---
+++ day53/func with return and without return.py
@@ -0,0 +1,22 @@
+## function without the return values
+
+def func(a,b):
+    result=a+b
+    print("result",result)
+
+
+func(1,4)
+
+
+
+
+
+## with using  thr return  value
+
+def function(a,b):
+    reslut=a*b
+    return reslut
+
+sum =function(2,4)
+print("result",sum)
+
