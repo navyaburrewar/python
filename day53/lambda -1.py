723183b6b6d297a21863cb13bf79428915dd6420
Navya <navyaburrewar2721@gmail.com>
2026-01-31 18:45:43 +0530

    lambda

---
+++ day53/lambda -1.py
@@ -0,0 +1,10 @@
+## Instead of writing a full function using def, you can write it shorter using lambda.
+## A lambda is just a small, quick function written in one line.
+#  A lambda function is a small anonymous function.
+
+## A lambda function can take any number of arguments, but can only have one expression.
+
+## syntax :
+
+##  lambda arguments : expression
+## The expression is executed and the result is returned:
