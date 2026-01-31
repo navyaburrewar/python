7a1082aea69aeaded18531c6f160ca359fec7f7b
Navya <navyaburrewar2721@gmail.com>
2026-01-31 18:55:55 +0530

    lambda

---
+++ day53/lambda-2.py
@@ -0,0 +1,15 @@
+##  adding  10 to a and  return reslut
+x=lambda a:a+10
+print(x(5))
+
+
+## adding two num
+y=lambda a,b : a+b
+print(y(2,5))
+
+# adding 3 number
+z=lambda a,b,c : a+b+c
+print(z(2,6,4))
+
+
+
