e0fddc9fd925176e2b1e7812f671b70ea144b54b
Navya <navyaburrewar2721@gmail.com>
2026-01-31 19:34:49 +0530

    lambda problem

---
+++ day53/lambda-5.py
@@ -0,0 +1,25 @@
+##   When You Need a Small Function Only Once
+
+
+##   noraml function  without using lambda
+
+def function(a,b):
+    result= a+b
+    print(result)
+function(3,5)    
+
+
+## using the lambda
+
+## syntax
+## lambda Arguments : expression
+
+add = lambda a,b : a+b
+print(add(40,20))
+
+
+
+
+
+
+    
