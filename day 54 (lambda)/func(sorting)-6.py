e2776cd21f53c3e1e3b926d1ef455c8f36652b6a
Navya <navyaburrewar2721@gmail.com>
2026-02-01 12:28:50 +0530

    sorting

---
+++ day 54 (lambda)/func(sorting)-6.py
@@ -0,0 +1,11 @@
+pairs=[(4,9),(8,6),(6,8)]
+sorting = sorted(pairs,key=lambda x :x[0])
+print(sorting)
+
+  ## above one is sorting  according to the index[0] 
+
+pairs=[(4,9),(8,6),(6,8)]
+sorting = sorted(pairs,key=lambda x :x[1])
+print(sorting)
+
+## above one is sorting  according to the index[1] 
