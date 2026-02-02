f21bfdc1852299809a24a1cbd5f732aacd9fdacb
Navya <navyaburrewar2721@gmail.com>
2026-02-02 22:47:21 +0530

    recursion

---
+++ day 55(recursion)/theory-1.py
@@ -0,0 +1,22 @@
+## recursion
+
+## Recursion is when a function calls itself.
+## Recursion is a common mathematical and programming concept. It means that a function calls itself. This has the benefit of meaning that you can loop through data to reach a result.
+
+# What is Recursion in Python?
+
+# Recursion is when a function calls itself to solve a smaller version of a problem.
+# Instead of using loops (for, while), recursion solves a problem by breaking it into smaller pieces of the same type.
+# Think of it like this:
+# “To solve a big problem, I’ll solve a smaller version of the same problem.”
+
+
+
+# Basic Structure of a Recursive Function
+
+# Every recursive function must have two main parts:
+# 1️⃣ Base Case
+# The condition that stops the recursion.
+# Without it → your program runs forever (infinite recursion).
+# 2️⃣ Recursive Case
+# The part where the function calls itself with a smaller input.
