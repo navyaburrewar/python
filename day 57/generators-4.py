25aae944ba446ee4f0c5c70f5340c118baf737b1
Navya <navyaburrewar2721@gmail.com>
2026-02-04 19:24:59 +0530

    generator

---
+++ day 57/generators-4.py
@@ -0,0 +1,22 @@
+##3 generators function
+
+
+# A generator is a special type of function that:
+# Returns values one by one
+# Instead of returning everything at once
+# Uses the keyword yield instead of return
+
+
+
+# Simple Way to Think About It
+
+# return → “Here is the final answer. I’m done.”
+# yield → “Here’s one value. I’ll give you more later.”
+
+
+# . yield (Generator Functions)
+
+# Turns the function into a generator.
+# Produces a value without ending the function.
+# Pauses execution and remembers its state.
+# Continues from where it left off when asked for the next value.
