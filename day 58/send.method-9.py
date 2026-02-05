56ae0aee48b210e69252d7c17d467e9fd82f5ac9
Navya <navyaburrewar2721@gmail.com>
2026-02-05 21:46:39 +0530

    send() method in generator

---
+++ day 58/send.method-9.py
@@ -0,0 +1,8 @@
+### send() generator
+
+# Normally, a generator only gives values out using yield.
+
+# But send() allows you to send a value INTO the generator while it is paused.
+# So:
+# next() → gets a value from generator
+# send(value) → sends a value into generator
