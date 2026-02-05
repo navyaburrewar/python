144e109d09c926518780f866df1f24ce15842216
Navya <navyaburrewar2721@gmail.com>
2026-02-05 19:20:36 +0530

    generator will saves the memory

---
+++ day 58/yield-3.py
@@ -0,0 +1,40 @@
+## generators will saves the memory
+## how it will saves the memory
+## real world example that that/
+
+
+# 1: Buffet vs Restaurant Kitchen
+# 🧺 List = Buffet Table
+
+
+# Imagine a buffet where:
+# The kitchen cooks 1000 dishes
+# All dishes are placed on the table at once
+# Now the table is full.
+# It needs a lot of space.
+# Even if you only eat 2 dishes, the other 998 are still sitting there.
+# That’s a list.
+# All values are created and stored.
+
+
+
+
+# 👨‍🍳 Generator = Restaurant Order System
+
+# Now imagine a normal restaurant:
+
+# You order one dish
+
+# The chef cooks that dish
+# You eat it
+# Then you order another
+# The kitchen does NOT cook 1000 dishes at once.
+# It only makes what is needed.
+# So it uses:
+
+# Less space
+# Less storage
+# Less waste
+# That’s a generator.
+
+
